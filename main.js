07781e5bdd839826f6c1104dcff1c0a87437058a
tyri41 <grzegorz.kwiatkowski41@gmail.com>
2020-04-28 20:27:33 +0200

    kroki 5: czas na czas i 6: obiecanki

--- main.js
+++
@@ -1,5 +0,0 @@
-var express = require('express');
-var app = express();
-app.listen(8080, function() {
-    console.log('server is running');
-});
---
+++ scripts/main.ts
@@ -0,0 +1,35 @@
+function getPromised(time: number) {
+    var promise = new Promise<void>((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, time);
+    });
+    return promise;
+}
+
+function setColor(color, el) {
+    console.log(color);
+    el.style.backgroundColor = color;
+}
+let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple', 'white'];
+function teczoweKolory(el: HTMLElement) {
+    let promise = getPromised(1000);
+    for (const color of colors) {
+        promise = promise.then(() => {
+            setColor(color, el); 
+            return getPromised(1000);
+        });
+    }
+}
+
+function getTSAvatar(el) {
+    fetch("https://api.github.com/repos/Microsoft/TypeScript/commits")
+        .then((response) => response.json())
+        .then((data) => {
+        console.log(data[0].author.avatar_url);
+        el.src = data[0].author.avatar_url;
+    })
+        .catch((data) => {
+        console.log(data);
+    });
+}
